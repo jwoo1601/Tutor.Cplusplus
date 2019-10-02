dcd31d1a9bd03996d96379064f53279259016540
Jeongwoo Kim <jkim504@myseneca.ca>
2019-10-02 02:01:12 -0400

    Updated Lecture03 Notes

---
+++ projects/lecture03/Lecture03-C.c
@@ -0,0 +1,97 @@
+﻿// 1. if-else if-else statement
+// expression 을 검사 -> true or false 로 평가될 수 있는 식
+// int a = 3;
+// a -> expression (0: false, 0이 아닌 값은 true)
+
+// Logical operator (논리 연산자) (operation 즉 연산식의 결과가 true 나 false)
+// opr1 == opr2 (상등 비교): opr1 이 opr2 와 같은지 (같으면 true, 다르면 false)
+// opr1 != opr2 : opr1이 opr2와 다른지 (다르면 ture, 같으면 false)
+// opr1 > opr2 : opr1 이 opr2보다 큰지 (크면 true, 크지 않으면 => 작거나 같으면 false)
+// opr1 < opr2 : opr1 이 opr2보다 작은지 (작으면 true, 크거나 같으면 false)
+// opr1 >= opr2 : opr1 이 opr2보다 크거나 같은지 (크거나 같으면 true, 작으면 false)
+// opr1 <= opr2 : opr1 이 opr2보다 작거나 같은지 (작거나 같으면 true, 크면 false)
+// opr1 && opr2 : opr1 과 opr2가 둘다 true 일때만 true 아니면 나머지 다 false
+// e.g. a > 3 && a < 5
+// opr1 || opr2 : opr1 과 opr2 둘중에 하나라도 true 면 true 즉 false 인 경우는 둘다 false 경우
+// e.g. a > 3 || a < 5
+// !opr : opr의 값을 뒤집기 (opr이 true면 false로, opr이 false면 true)
+// !(a > 3) : a 가 3보다 크지 않다 즉, a 가 3보다 작거나 같다
+// !a = false로 평가 (a가 1일때) (a의 값을 변경하진 않음)
+
+// if statement
+// if (expression)
+// {
+//		...
+// }
+
+// e.g. if (a > 5)  if (!a) -> !0 -> !false -> true
+//		{
+//			a = 3;
+//		}
+
+// if (expression)
+//	{
+//		...
+//	}
+// else
+// {
+//		...
+// }
+
+// if (expression)  (조건이 여러개)
+// {
+//		...
+// }
+// else if (expression)
+// {
+//		...
+// }
+// ...
+// else
+// {
+//		...
+// }
+
+// switch ~ case statement (조건은 하나, 다만 그 조건의 값이 여러개일때)
+/*
+	switch (expression)  // expression 의 평가값이 정수여야만 함 -> true/false, int, short, char, long
+	{
+		case <value1>:
+		...
+		break;
+
+		case <value2>:
+		...
+		break;
+		
+		...
+
+		default:
+		...
+		break;
+	}
+*/
+
+// for 문은 정해진 횟수만큼 반복할 때
+// for (<init>;expression;증감문) // 증감문
+// {
+//		반복할 코드
+// }
+
+// for (int i =0; i < size; i++)
+// for (int i =1; i <= size; i++)
+// for (int i =5; i > 0; i--)
+// for (int i =4; i >=0; i--)
+// 전부다 실행횟수: 5번
+
+// while 문은 조건을 만족하는 동안 반복할 때
+// while (expression)     -> expression 이 true 일 경우에 반복
+// {
+//		반복할코드
+// }
+
+// a = 0;
+// while (a < 5)
+// {
+//		a = a + 1;
+// }
---
+++ projects/lecture03/Lecture03-CPP.cpp
@@ -0,0 +1,90 @@
+﻿/*
+	C -> 절차 지향 언어 (Procedural Language)
+	: 동작들의 나열로 설명
+	엔진을 점화한다
+	엑셀을 밟는다
+	브레이크를 밟는데
+
+	C++ -> 객체 지향 언어 (Object Oriented Language)
+	: 자동차에는 바퀴가 있다
+		바퀴는 구른다
+	  자동차에는 핸들이 있다
+		핸들을 돌리면 방향을 바꾼다
+	  자동차에는 브레이크가 있다
+		브레이크는 밟으면 멈춘다
+
+	class -> classification : 분류
+	e.g. 바퀴
+		 엔진
+		 핸들
+		 와이퍼
+		 유리창
+		 디스플레이
+*/
+
+// class <identifier>
+// {
+//		멤버 함수 (instance function = member functions = method)
+//		멤버 변수 (member variable = instance variable = field)
+// };
+
+class Test
+{
+	int a;
+
+	void func1();
+	int func2();
+	short func3();
+};
+
+// 클래스의 멤버함수는 해당 클래스의 멤버 변수에 접근을 할 수있음
+
+void Test::func1()
+{
+	a = 3;
+}
+
+// <return-type> <identifier>(<parameter>);    -> 이 함수의 signature or 머리 
+// {
+// }
+
+// opr1::opr2 -> scope resolution operator  -> opr2 가 opr1 에 속해있다는 소속을 밝힘
+
+// function -> 동작을 정의
+// 손을 흔든다
+// 밥을 먹는다
+// 화장실에 간다
+
+//variable을 만들어라
+// a에다 3을 대입해라
+// a 가 4보다 크면 ~~해라
+
+// function 의 input: parameter (매개변수) or argument (인수)
+// function 의 output: return value (리턴값)
+// <return-type> <identifier>(<parameter1-type> <param1-name>, <param2-type> <param2-name>...)
+// {
+//		코드
+// }
+
+// 두 파라미터를 더해서 더한값을 리턴하는 function
+int add(int a, int b)
+{
+	return a + b;
+}
+
+// 값을 내보내기: return expression;
+
+// function 을 사용하는 법 (function call or 함수 호출)
+// call __ function
+// <identifier>(<parameter>);
+
+int main()
+{
+	int c = add(5, 3);
+	add(c - 5, 1);
+}
+
+// 아무곳에도 소속되있지 않은 범위 -> global scope
+// function 안에 소속되면 -> function scope / local scope
+// class 안에 소속되면 -> class scope
+// code block 안에 소속되면 -> block scope
